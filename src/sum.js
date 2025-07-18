ee43b936470847d6a063dcd2e8cc427544ae9f28
yoon00 <y00ndlek@gmail.com>
2025-07-18 14:47:48 +0900

    modif

--- src/sum.js
+++ src/sum.js
@@ -1,5 +1,8 @@
-function sum(a, b){
-    return a + b;
+
+function sum(a,b){
+  return a + b;
 }
+
+
 
 module.exports = {sum};
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,5 +1,7 @@
+
 const {sum} = require('../src/sum');
 
-test('1 + 2 to equal 3', () => {
-    expect(sum(1, 2).toBe(3));
+
+test('1 + 2 to equal 3',() => {
+    expect(sum(1, 2)).toBe(3);
 })
